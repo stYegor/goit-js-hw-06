8fa3b11ce55a55f5a5f599b2ad51741fe5b16f81
stYegor <storchay94@gmail.com>
2022-10-02 21:01:18 +0300

    redo task 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,29 +1,11 @@
 const allCategories = document.querySelectorAll(".item");
 console.log("Number of categories: ", allCategories.length);
 
-//////
-
-const firstCard = document.querySelector(".item");
-const firstItem = document.querySelector("h2");
-console.log("Category:", firstItem.textContent);
-
-const firstItemSibl = firstItem.nextElementSibling.children;
-console.log("Elements: ", firstItemSibl.length);
-
-//////
-
-const secondCard = firstCard.nextElementSibling;
-const secondItem = secondCard.firstElementChild;
-console.log("Category:", secondItem.textContent);
-
-const secondItemSibl = secondItem.nextElementSibling.children;
-console.log("Elements: ", secondItemSibl.length);
-
-//////
-
-const lastCard = secondCard.nextElementSibling;
-const lastItem = lastCard.firstElementChild;
-console.log("Category:", lastItem.textContent);
-
-const lastItemSibl = lastItem.nextElementSibling.children;
-console.log("Elements: ", lastItemSibl.length);
+const nameOfCategoties = document.querySelectorAll("h2");
+nameOfCategoties.forEach(function (nameOfCategoties) {
+  console.log("Category: ", nameOfCategoties.textContent);
+  console.log(
+    "Elements: ",
+    nameOfCategoties.nextElementSibling.childElementCount
+  );
+});
